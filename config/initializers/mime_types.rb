1472214970f951e63afd13283febe345b27063a7
Benjamin Heinrich <ben.heinrich@live.com>
2012-09-20 18:15:33 +0200

    wav added

--- config/initializers/mime_types.rb
+++ config/initializers/mime_types.rb
@@ -6,6 +6,6 @@
 Rack::Mime::MIME_TYPES.merge!({
   ".ogg"  => "application/ogg",
   ".oga"  => "audio/ogg",
-  ".ogx"  => "application/ogg"
+  ".ogx"  => "application/ogg",
   ".wav"  => "audio/wav"
   })
